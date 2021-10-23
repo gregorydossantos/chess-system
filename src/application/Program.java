ac03ad8b5c3b8bb5db268584d3b10425f7808d68
Gregory Castezana <gregescribano@gmail.com>
2021-10-23 16:24:14 -0300

    First class in project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello wolrd!");
+        Position pos = new Position(1, 2);
+        System.out.println(pos.toString());
     }
 }
